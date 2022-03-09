b6e9b9c6a4265ca023d5d3b2f1b9cc02f71cf31b
BMB <bruno.marquez@ucb.edu.bo>
2022-03-09 17:45:13 -0400

    6to commit

--- src/presenter.js
+++ src/presenter.js
@@ -9,7 +9,9 @@ const precio_item = document.querySelector("#precio-item");
 const div_precio_item = document.querySelector("#precio-item-div");
 
 const codigo_estado = document.querySelector("#codigo-estado");
-const div_codigo_estado = document.querySelector("#codigo-item-div"); 
+const div_codigo_estado = document.querySelector("#codigo-estado-div"); 
+
+const div_impuesto_estado = document.querySelector("#impuesto-estado-div");
 
 const div_precio_neto = document.querySelector("#precio-neto-div");
 
@@ -22,6 +24,7 @@ form.addEventListener("submit", (event) => {
   div_precio_item.innerHTML = "<p> Precio de item: " + precio_item.value + "</p>";
 
   div_codigo_estado.innerHTML = "<p> Codigo del Estado de "+ codigo_estado.value +": " + porcentaje_estado(codigo_estado.value) + "%</p>";
+  div_impuesto_estado.innerHTML = "<p> Monto de Impuesto para el Estado de "+ codigo_estado.value +": " + monto_impuesto_estado(codigo_estado.value, precio_neto) + "</p>";
   
   div_precio_neto.innerHTML = "<p> Precio Neto: " + precio_neto + "</p>";
 
@@ -35,5 +38,12 @@ function porcentaje_estado(estado) {
     case 'TX': return 6.25;break;
     case 'AL': return 4; break;
     case 'CA': return 8.25;break;
+  }
+}
+
+function monto_impuesto_estado(estado, monto)
+{
+  switch(estado){
+    case 'CA': return (8.25 * monto)/100;break;
   }
 }
